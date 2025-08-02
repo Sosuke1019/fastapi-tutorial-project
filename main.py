591e262aa89f8438c7d1474ef671bfe95205d795
Sosuke <kato.052wazime@gmail.com>
2025-08-02 20:56:02 +0900

    パスパラメータ

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from fastapi-tutorial-project!")
+from enum import Enum
+from fastapi import FastAPI
 
+class ModelName(str, Enum):
+    alexnet= "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+@app.get("/models/{model_name}") # パスオペレーションデコレータを記述
+async def get_model(model_name: ModelName): # パスオペレーション関数を定義
+    if model_name is ModelName.alexnet:
+        return {"mode_name": model_name, "message": "Deep Learning FTW!"}
+    
+    if model_name.value == "resnet":
+        return {"mode_name": model_name, "message": "LeCNN all the images"}
+    
+    return {"mode_name": model_name, "message": "Have some residuals"}
+
+@app.get("/files/{path:path}")
+async def read_file(path: str):
+    return {"file_path": path}
