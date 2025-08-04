164c57d3c2480fcc556be9946603db2e2d2b1aca
Sosuke <kato.052wazime@gmail.com>
2025-08-05 08:50:50 +0900

    クエリパラメータモデル

--- main.py
+++ main.py
@@ -1,59 +1,74 @@
 from enum import Enum
-from typing import Annotated
 from fastapi import FastAPI, Query
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing_extensions import Annotated, Literal
 
-class ModelName(str, Enum):
-    alexnet= "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+# class ModelName(str, Enum):
+#     alexnet= "alexnet"
+#     resnet = "resnet"
+#     lenet = "lenet"
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
 
 app = FastAPI()
 
-# クエリパラメータと文字列の検証
+# クエリパラメータモデル
+class FilterParams(BaseModel):
+    model_config = {"extra": "forbid"}
+
+    limit: int = Field(100, gt=0, le=100)
+    offset: int = Field(0, ge=0)
+    order_by: Literal["created_at", "updated_at"] = "created_at"
+    tags: list[str] = []
+
 @app.get("/items/")
-async def read_items(q: Annotated[str, Query(min_length=3)]):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
+async def read_items(filter_query: Annotated[FilterParams, Query()]):
+    return filter_query
 
 
-# リクエストボディ
-@app.post("/items/{item_id}")
-# パラメータがPydanticモデル型で宣言された場合、リクエストボディとして解釈される
-async def create_item(item_id: int, item: Item, q: str | None = None):
-    item_dict = item.dict()
-    if item.tax is not None:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
+# パスパラメータと数値の検証
 
-# クエリパラメータ
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+# # クエリパラメータと文字列の検証
+# @app.get("/items/")
+# async def read_items(q: Annotated[str, Query(min_length=3)]):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
 
-@app.get("/users/{item_id}")
-async def read_user_item(item_id: str, user_id: str, q: str | None = None):
-    item = {"アイテム番号": item_id, "ユーザー番号" :user_id, "option": q}
-    return item
+# # リクエストボディ
+# @app.post("/items/{item_id}")
+# # パラメータがPydanticモデル型で宣言された場合、リクエストボディとして解釈される
+# async def create_item(item_id: int, item: Item, q: str | None = None):
+#     item_dict = item.dict()
+#     if item.tax is not None:
+#         price_with_tax = item.price + item.tax
+#         item_dict.update({"price_with_tax": price_with_tax})
+#     return item_dict
 
-# パスパラメータ
-@app.get("/models/{model_name}") # パスオペレーションデコレータを記述
-async def get_model(model_name: ModelName): # パスオペレーション関数を定義
-    if model_name is ModelName.alexnet:
-        return {"mode_name": model_name, "message": "Deep Learning FTW!"}
+# # クエリパラメータ
+# fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+# @app.get("/users/{item_id}")
+# async def read_user_item(item_id: str, user_id: str, q: str | None = None):
+#     item = {"アイテム番号": item_id, "ユーザー番号" :user_id, "option": q}
+#     return item
+
+# # パスパラメータ
+# @app.get("/models/{model_name}") # パスオペレーションデコレータを記述
+# async def get_model(model_name: ModelName): # パスオペレーション関数を定義
+#     if model_name is ModelName.alexnet:
+#         return {"mode_name": model_name, "message": "Deep Learning FTW!"}
     
-    if model_name.value == "resnet":
-        return {"mode_name": model_name, "message": "LeCNN all the images"}
+#     if model_name.value == "resnet":
+#         return {"mode_name": model_name, "message": "LeCNN all the images"}
     
-    return {"mode_name": model_name, "message": "Have some residuals"}
+#     return {"mode_name": model_name, "message": "Have some residuals"}
 
-@app.get("/files/{path:path}")
-async def read_file(path: str):
-    return {"file_path": path}
+# @app.get("/files/{path:path}")
+# async def read_file(path: str):
+#     return {"file_path": path}
