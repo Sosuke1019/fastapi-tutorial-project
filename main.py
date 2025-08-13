e78c6d0e4f36dae866d18cceebd856a1cf950af5
Sosuke <kato.052wazime@gmail.com>
2025-08-13 18:22:20 +0900

    モデル -より詳しく

--- main.py
+++ main.py
@@ -1,11 +1,72 @@
+from typing import Union, Dict
 from enum import Enum
 from datetime import datetime, time, timedelta
 from uuid import UUID
 from fastapi import FastAPI, Query, Body, Cookie, Header
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 from typing_extensions import Annotated, Literal
 
 app = FastAPI()
+
+# # モデル -より詳しく
+# class UserBase(BaseModel):
+#     username: str
+#     email: EmailStr
+#     full_name: str | None = None
+
+# class UserIn(UserBase):
+#     password: str
+
+# class UserOut(UserBase):
+#     pass
+
+# class UserInDB(BaseModel):
+#     hashed_password: str
+
+# def fake_password_hasher(raw_password: str):
+#     return "supersecret" + raw_password
+
+# def fake_save_user(user_in: UserIn):
+#     hashed_password = fake_password_hasher(user_in.password)
+#     # user_in.dict()でPydanticモデルを辞書に変換。**で辞書を関数の引数として展開。
+#     # ※ UserIn には password フィールドがあるが、UserInDB には hashed_password フィールドしかない。**user_in.dict() で展開された password は、UserInDB のフィールドに存在しないため無視される。
+#     #  ** （アスタリスク2つ）: Pythonの「辞書展開」（dictionary unpacking）演算子。辞書のキーと値のペアを、関数の引数として展開する。
+#     user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+#     print("User saved! ..not really")
+#     return user_in_db
+
+# @app.post("/user/", response_model=UserOut)
+# async def create_user(user_in: UserIn):
+#     user_saved = fake_save_user(user_in)
+#     return user_saved
+
+# class BaseItem(BaseModel):
+#     description: str
+#     type: str
+
+# class CarItem(BaseItem):
+#     type: str = "car"
+
+# class PlaneItem(BaseItem):
+#     type: str = "plane"
+#     size: int
+
+# items = {
+#     "item1": {"description": "All my friends drive a low rider", "type": "car"},
+#     "item2": {
+#         "description": "Music is my aeroplane, it's my aeroplane",
+#         "type": "plane",
+#         "size": 5,
+#     },
+# }
+
+# @app.get("/items/{imte_id}", response_model=Union[PlaneItem, CarItem])
+# async def read_item(item_id: str):
+#     return items[item_id]
+
+# @app.get("/keyword_weights/", response_model=dict[str, float])
+# async def read_keyword_weights():
+#     return {"foo": 2.3, "bar": 3.4}
 
 # # レスポンスモデル
 # class Item(BaseModel):
