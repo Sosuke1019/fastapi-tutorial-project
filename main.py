86b4c03812dedad3d4d963f5f7fd642bd4ac2632
Sosuke <kato.052wazime@gmail.com>
2025-08-13 16:18:43 +0900

    レスポンスモデル

--- main.py
+++ main.py
@@ -7,6 +7,37 @@ from typing_extensions import Annotated, Literal
 
 app = FastAPI()
 
+# # レスポンスモデル
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float = 10.5
+#     tags: list[str] = []
+
+# items = {
+#     "foo": {"name": "Foo", "price": 50.2},
+#     "bar": {"name": "Bar", "description": "The Bar fighters", "price": 62, "tax": 20.2},
+#     "baz": {
+#         "name": "Baz",
+#         "description": "There goes my baz",
+#         "price": 50.2,
+#         "tax": 10.5,
+#     },
+# }
+
+# @app.get(
+#     "/items/{item_id}/name",
+#     response_model=Item,
+#     response_model_include={"name", "description"},
+# )
+# async def read_item_name(item_id: str):
+#     return items[item_id]
+
+# @app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"})
+# async def read_item(item_id: str):
+#     return items[item_id]
+
 # # ヘッダーパラメータモデル
 # class CommonHeaders(BaseModel):
 #     model_config = {"extra": "forbid"}
