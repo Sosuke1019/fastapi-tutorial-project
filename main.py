1639d07008c0f4b467b782f5a99d2cfd11e54cf7
Sosuke <kato.052wazime@gmail.com>
2025-08-14 19:34:29 +0900

    リクエストファイル

--- main.py
+++ main.py
@@ -2,13 +2,37 @@ from typing import Union, Dict
 from enum import Enum
 from datetime import datetime, time, timedelta
 from uuid import UUID
-from fastapi import FastAPI, Query, Body, Cookie, Header, Form
+from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Query, Body, Cookie, Header, Form, File, UploadFile
 from pydantic import BaseModel, Field, EmailStr
 from typing_extensions import Annotated, Literal
 
 app = FastAPI()
 
+# # リクエストファイル
+# @app.post("/files?")
+# async def create_file(files: Annotated[list[bytes], File(description="A file read as bytes")]):
+#     return {"file_size": [len(file) for file in files]}
 
+# @app.post("/uploadfile/")
+# async def create_upload_file(files: Annotated[list[UploadFile], File(description="A file read as UploadFile")]):
+#     return {"filename": [file.filename for file in files]}
+
+# @app.get("/")
+# async def main():
+#     content = """
+# <body>
+# <form action="/files/" enctype="multipart/form-data" method="post">
+# <input name="files" type="file" multiple>
+# <input type="submit">
+# </form>
+# <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+# <input name="files" type="file" multiple>
+# <input type="submit">
+# </form>
+# </body>
+#     """
+#     return HTMLResponse(content=content)
 
 # # フォームモデル
 # class FormData(BaseModel):
