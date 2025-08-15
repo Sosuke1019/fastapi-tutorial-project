c422a124d7e47afea68d7d727b8753f587bcd33a
Sosuke <kato.052wazime@gmail.com>
2025-08-15 20:49:06 +0900

    コメントアウト

--- main.py
+++ main.py
@@ -17,31 +17,31 @@ from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 
-# Path Operationの設定
-# Path Operationデコレータにパラメータを渡すことで、Path Operationのメタデータを簡単に設定・追加できる
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-    tags: set[str] = set()
+# # Path Operationの設定
+# # Path Operationデコレータにパラメータを渡すことで、Path Operationのメタデータを簡単に設定・追加できる
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+#     tags: set[str] = set()
 
-@app.post("/items/", response_model=Item, tags=["items"], summary="Create an item", response_description="The created item")
-async def create_item(item: Item):
-    """
-    Create an item with all the information:
+# @app.post("/items/", response_model=Item, tags=["items"], summary="Create an item", response_description="The created item")
+# async def create_item(item: Item):
+#     """
+#     Create an item with all the information:
 
-    - **name**: each item must have a name
-    - **description**: a long description
-    - **price**: required
-    - **tax**: if the item doesn't have tax, you can omit this
-    - **tags**: a set of unique tag strings for this item
-    """
-    return item
+#     - **name**: each item must have a name
+#     - **description**: a long description
+#     - **price**: required
+#     - **tax**: if the item doesn't have tax, you can omit this
+#     - **tags**: a set of unique tag strings for this item
+#     """
+#     return item
 
-@app.get("/elements/", tags=["items"], deprecated=True)
-async def read_elements():
-    return [{"item_id": "Foo"}]
+# @app.get("/elements/", tags=["items"], deprecated=True)
+# async def read_elements():
+#     return [{"item_id": "Foo"}]
 
 # # エラーハンドリング
 # class UnicornException(Exception):
