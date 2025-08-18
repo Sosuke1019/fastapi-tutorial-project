7c7fa316b65199ef1205d473b1fccb731094b640
Sosuke <kato.052wazime@gmail.com>
2025-08-18 12:32:25 +0900

    サブ依存関係

--- main.py
+++ main.py
@@ -18,6 +18,24 @@ from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 
+# # サブ依存関係
+# def query_extractor(q: str | None = None):
+#     return q
+
+# def query_or_cookie_extractor(
+#     q: Annotated[str, Depends(query_extractor)],
+#     last_query: Annotated[str|None, Cookie()] = None
+# ):
+#     if not q:
+#         return last_query
+#     return q
+
+# @app.get("/items/")
+# async def read_query(
+#     query_or_default: Annotated[str, Depends(query_or_cookie_extractor)],
+# ):
+#     return {"q_or_cookie": query_or_default}
+
 # # 依存関係としてのクラス
 # fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
