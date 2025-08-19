0cfe23553aa9e465d55c2a748e8568cc85227fba
Sosuke <kato.052wazime@gmail.com>
2025-08-19 15:18:46 +0900

    現在のユーザーの取得

--- main.py
+++ main.py
@@ -19,6 +19,28 @@ from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 
+# # 現在のユーザーの取得
+# oauth2_sheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# class User():
+#     username: str
+#     email: str | None = None
+#     full_name: str | None = None
+#     disabled: bool | None = None
+
+# def fake_decode_token(token):
+#     return User(
+#         username = token + "fakedecoded", email ="john@example.com", full_name="John Doe"
+#     )
+
+# async def get_current_user(token: Annotated[str, Depends(oauth2_sheme)]):
+#     user = fake_decode_token(token)
+#     return user
+
+# @app.get("/users/me")
+# async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
+#     return current_user
+
 # # セキュリティ -最初の一歩
 # oauth2_sheme = OAuth2PasswordBearer(tokenUrl="token")
 
